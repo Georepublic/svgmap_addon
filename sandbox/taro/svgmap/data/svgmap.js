0fd620ab0a09895c355c6d9c2439731c5a5e4a1b
Taro Matsuzawa <taro@georepublic.co.jp>
2012-11-21 20:47:58 +0900

    add zoomup, zoomdown button

--- sandbox/taro/svgmap/data/svgmap.js
+++ sandbox/taro/svgmap/data/svgmap.js
@@ -3,22 +3,23 @@ self.port.on("getElement", function() {
     console.log(window.innerWidth);
     for (var i = 0; i < elements.length; i++) {
 	//self.port.emit("gotElement", elements[i].width.baseVal.value);
-	new SVGMapParser(elements[i]);
+	new SVGMapParser(elements[i], true);
     }
 });
 
-function SVGMapParser(element)
+function SVGMapParser(element, isRoot)
 {
     console.log("SVGMapParser");
-    this.load(element);
+    this.load(element, isRoot);
 }
 
 SVGMapParser.prototype = {
-    load : function(element) {
+    load : function(element, isRoot) {
 	console.log("load");
 	this.rootDocument = element;
 	this.mapx = 138;
 	this.mapy = 37;
+	this.zoomRatio = 1.41;
 	this.mapCanvas = null;
 	this.mapCanvasSize = null;
 	this.rootViewPort = null;
@@ -28,7 +29,7 @@ SVGMapParser.prototype = {
 	this.svgImagesCRS = new Array();
 	
 	this.mapCanvasSize = this.canvasSize(this.rootDocument);
-
+	this.createZoomButton(this.rootDocument);
 	var docId = "root";
 	var docPath = "/";
 	this.loadElement(element, docId, docPath);
@@ -52,5 +53,29 @@ SVGMapParser.prototype = {
 	    width: w,
 	    height: h
 	}
-    }
+    },
+
+    createZoomButton : function(element) {
+	var upButton = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	upButton.setAttribute("d", "M 0 4 L 4 0 L 8 4 L 6 4 L 6 8 L 2 8 L 2 4 z");
+	upButton.setAttribute("stroke", "black");
+	upButton.setAttribute("fill", "white");
+	upButton.setAttribute("id", "zoomup");
+	element.appendChild(upButton);
+	upButton.addEventListener("click", this.zoomup, false);
+	var downButton = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	downButton.setAttribute("d", "M 0 14 L 4 18 L 8 14 L 6 14 L 6 10 L 2 10 L 2 14 z");
+	downButton.setAttribute("stroke", "black");
+	downButton.setAttribute("fill", "white");
+	downButton.setAttribute("id", "zoomdown");
+	element.appendChild(downButton);
+	downButton.addEventListener("click", this.zoomdown, false);
+    },
+
+    zoomup : function(event) {
+	console.log("zoomup");
+    },
+    zoomdown : function(event) {
+	console.log("zoomdown");
+    },
 };
