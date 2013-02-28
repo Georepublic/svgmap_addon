f8aa0914a6ee3243d701d5dac2357d63d4fbaf49
Ko Nagase <nagase@georepublic.co.jp>
2013-02-28 10:57:36 +0900

    add gcs log and test content.

--- sandbox/htmliframe/data/htmliframe.js
+++ sandbox/htmliframe/data/htmliframe.js
@@ -1,6 +1,11 @@
 console.log("applied htmlframe.js");
 replaceTagName("iframe", "g");
+logGCS(document);
 document.addEventListener("click", function(evt) { replaceTagName("g", "iframe"); }, false);
+if (window.parent != window) {
+	console.log("parent exists");
+	logGCS(window.parent.document);
+}
 
 function replaceTagName(fromName, toName) {
 	var fromNodes = document.getElementsByTagName(fromName);
@@ -14,5 +19,13 @@ function replaceTagName(fromName, toName) {
 			}
 		}
 		fromNode.parentNode.replaceChild(toNode, fromNode);
+	}
+}
+
+function logGCS(doc) {
+	var gcss = doc.getElementsByTagName("globalCoordinateSystem");
+	if (gcss.length > 0) {
+		var tr = gcss[0].getAttribute("transform");
+		console.log(tr);
 	}
 }
